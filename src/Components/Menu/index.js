4776d7985524bf84b70a6852f0dcc7fbceeb1999
danielhawkesford <daniel.hawkesford@gmail.com>
2022-09-09 10:48:26 +0100

    Added map to menu component

--- src/Components/Menu/index.js
+++ src/Components/Menu/index.js
@@ -1,35 +1,49 @@
 import { useEffect, useState } from "react";
 
-
 function Menu() {
-    // const [recipes, setRecipes] = useState([]);
-    const [basketItems, setBasketItems] = useState([]);
-    useEffect(() => {
-        async function getRecipesOnLoad() {
-            let response = await fetch(
-                "https://api.spoonacular.com/recipes/complexSearch?apiKey=4f148a4f43724f62aa67a95e9335cd75&cuisine=thai&number=50"
-            );
-            console.log("response: ", response);
-            let data = await response.json();
-            console.log("data: ", data);
-            let results = data.results;
-            console.log("results: ", results);
-            return results;
-        }
-    });
-    function addtoBasket(itemName) {
-        console.log(basketItems);
-        setBasketItems([...basketItems, itemName]);
+  const [recipes, setRecipes] = useState([]);
+  const [basketItems, setBasketItems] = useState([]);
+  useEffect(() => {
+    async function getRecipesOnLoad() {
+      let response = await fetch(
+        "https://api.spoonacular.com/recipes/complexSearch?apiKey=4f148a4f43724f62aa67a95e9335cd75&cuisine=thai&number=50"
+      );
+      console.log("response: ", response);
+      let data = await response.json();
+      console.log("data: ", data);
+      let results = data.results;
+      console.log("results: ", results);
+      setRecipes(results);
+      return results;
     }
-    return (
-        <>
-            <main>
-                <h2>Menu</h2>
-                <p>That feels like an existential question, don't you think?</p>
-            </main>
+    getRecipesOnLoad();
+  }, []);
 
-        </>
-    );
+  function addtoBasket(itemName) {
+    console.log(basketItems);
+    setBasketItems([...basketItems, itemName]);
+  }
+  return (
+    <>
+      <main>
+        <h2>Menu</h2>
+        <div>
+          {recipes ? (
+            recipes.map((recipe) => {
+              return (
+                <div>
+                  <h2>{recipe.title}</h2>
+                  <img src={recipe.image} />
+                </div>
+              );
+            })
+          ) : (
+            <></>
+          )}
+        </div>
+      </main>
+    </>
+  );
 }
 
 export default Menu;
