f16dfae3a754d08b11ef538813c7d3a2513fadd5
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-09-09 14:50:27 +0100

    NOT WORKING React-testing-library tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { MemoryRouter, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+/* test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+}); */
+
+describe("App testing", () => {
+    test("Heading appears", () => {
+        
+        render(<App />);
+        let title = screen.getByLabelText(
+            "Welcome to our tantalising Thai takeout !"
+        );
+        expect(title).toBeInTheDocument();
+    });
+    test("Menu link appears", () => {
+        render(<App />);
+        let MenuLink = screen.getByText("Menu");
+        expect(MenuLink).toBeInTheDocument();
+    });
+    test("Menu link works", async () => {
+        // const user = userEvent.setup()
+        let history = createMemoryHistory();
+        history.push = jest.fn();
+        render(
+            <MemoryRouter location = {history.location} navigator={history}>
+                <App />
+            </MemoryRouter>
+        );
+        let MenuLink = screen.getByText("Menu");
+        await userEvent.click(MenuLink)
+        expect(history.push).toHaveBeenCalledWith("/menu")
+        /* renderWithRouter(<App/>) */
+    });
+    test("Basket link appears", () => {
+        render(<App />);
+        let Basket = screen.getByLabelText("Basket");
+        expect(Basket).toBeInTheDocument();
+    });
 });
