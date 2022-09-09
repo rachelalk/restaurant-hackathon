e3cfe54a6240971ff6b09e0c73ce36a3789b1e15
danielhawkesford <daniel.hawkesford@gmail.com>
2022-09-09 14:39:39 +0100

    Added CSS

--- src/Components/Homepage/index.js
+++ src/Components/Homepage/index.js
@@ -1,13 +1,31 @@
-
+import bg from './bg.jpg';
+import { Routes, Route, Link } from "react-router-dom";
 
 
 function Homepage() {
 	return (
-		<>
-			<main>
-				<h2>Homepage</h2>
+		// <>
+			<main className="home-bg">
+					<h1>
+						Opening hours
+						<br>
+
+						</br>Mon - Fri: 9am - 5pm
+						<br></br>
+						Sat - Sun: Closed
+					</h1>
+					<h1>
+						Address
+						<br>
+						</br>
+						Custard Factory, Digbeth
+						<br>
+						</br>
+						Birmingham
+					</h1>
+
 			</main>
-		</>
+	/* </> */
 	);
 }
 
