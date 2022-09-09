daae87d65dfc6d922e451a308a4702817e497189
rachelalk <100844754+rachelalk@users.noreply.github.com>
2022-09-09 14:55:58 +0100

    updated tests to suit styling changes

--- cypress/e2e/restaurant-e2e/restaurant.cy.js
+++ cypress/e2e/restaurant-e2e/restaurant.cy.js
@@ -3,23 +3,18 @@ describe("Load webpage", () => {
 		cy.visit("http://localhost:3000/");
 	});
 
-	it("displays the title 'Welcome to our tantalising Thai takeout !'", () => {
-		cy.get("h1").should(
-			"have.text",
-			"Welcome to our tantalising Thai takeout !"
-		);
-	});
 
 	it("can click onto the menu page, add 2 things to the basket and view modals, it can then visit the basket, navigate away from the page and back with the data persisting", () => {
 		cy.contains("Menu").click();
 
-		cy.get(":nth-child(1) > .css-1zb9ui").click();
-
-		cy.get(":nth-child(2) > .css-1qjb353").click();
+		cy.get(":nth-child(2) > .modal-button").click();
 
 		cy.get(".chakra-modal__footer > .css-1zb9ui").click();
 
-		cy.get(".chakra-modal__footer > .css-1qjb353").click();
+		cy.get(".css-1qjb353").click();
+
+		cy.get(":nth-child(5) > .css-1zb9ui").click();
+
 
 		cy.get('[href="/basket"]').click();
 
@@ -27,7 +22,4 @@ describe("Load webpage", () => {
 
 		cy.get('[href="/basket"]').click();
 	});
-
-
 });
-
